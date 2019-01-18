cc89c015ec922e9848cc3204027d0b3ba9ad3b2a
Amy Haddad <amy.m.haddad@gmail.com>
2019-01-18 09:16:21 -0500

    adding quicksort.py

--- divide_conquer.py
+++ divide_conquer.py
@@ -1,12 +1,12 @@
 #4.1
 
-# list = [1, 2, 3, 4]
+list = [1, 2, 3, 4]
 
-# def sum(list):
-#     if list == []:
-#         return 0
-#     else:
-#         return list[0] + sum(list[1:])
+def sum(list):
+    if list == []:
+        return 0
+    else:
+        return list[0] + sum(list[1:])
 
 # print(sum(list))
 
@@ -23,7 +23,8 @@
 #         return 1 + count(list[1:])
 
 # x = count(list)
-# # print(x)
+
+# print(x)
 
 # def countdown(x):
 #     print(x)
@@ -46,7 +47,7 @@
 
 # x = count(list)
 
-# list = [2, 4, 6]
+list = [2, 4, 6]
 
 def max(list):
     #base case: if length of list is 2:
@@ -57,13 +58,17 @@ def max(list):
 
     #recursive call to function via a variable, 
     #recursive call uses max, which is a built-in function, starting at value 1 and continues through the len of the list
+    #use list[1:] b/c in the next line of code we set up a comparison with the first value of list (list[0]) and compare this value to the rest of the list
+    #remember: D&C is about moving closer to an empty array with each recursive call 
     sub_max = max(list[1:])
-    import pdb; pdb.set_trace()
+
     #set up comparison: return list[0] if list[0] is greater than submax; otherwise return sub_max
     return list[0] if list[0] > sub_max else sub_max
 
     #In this ex, list[0] > sub_max so return sub_max and go thru the function again and stop at the first conditional, since len(list) now == 2
 x = max(list)
+
+print(x)
 
 
 
